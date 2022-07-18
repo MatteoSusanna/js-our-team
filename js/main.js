af33a14b2c5c7b9e4d35868b2bea624473cd2d14
Matteo Susanna <matteo2784@gmail.com>
2022-07-18 14:27:27 +0200

    Fine esercizio

--- js/main.js
+++ js/main.js
@@ -1,35 +1,3 @@
-/*
-Utilizzando i dati forniti, creare un array di 
-oggetti per rappresentare i membri del team.
-Ogni membro è caratterizzato dalle seguenti informazioni: nome, ruolo e foto.
-MILESTONE 0:
-Creare l’array di oggetti con le informazioni fornite.
-MILESTONE 1:
-Stampare su console le informazioni di nome, ruolo e la stringa della foto
-MILESTONE 2:
-Stampare le stesse informazioni su DOM sottoforma di stringhe
-BONUS 1:
-Trasformare la stringa foto in una immagine effettiva
-BONUS 2:
-Organizzare i singoli membri in card/schede
-Consigli del giorno:
-Ragioniamo come sempre a step.
-Prima la logica in italiano e poi traduciamo in codice.
-E ricordiamoci che console.log() è nostro amico!
-Buon lavoro!
-*/ 
-
-
-/*
-NOME                  RUOLO                        IMMAGINE
-Wayne Barnett	      Founder & CEO	               wayne-barnett-founder-ceo.jpg
-Angela Caroll	      Chief Editor	               angela-caroll-chief-editor.jpg
-Walter Gordon	      Office Manager	           walter-gordon-office-manager.jpg
-Angela Lopez	      Social Media Manager	       angela-lopez-social-media-manager.jpg
-Scott Estrada	      Developer	                   scott-estrada-developer.jpg
-Barbara Ramos	      Graphic Designer	           barbara-ramos-graphic-designer.jpg 
-*/
-
 const teamArray =   [
                         {
                             'nome': 'Wayne Barnett',
@@ -73,11 +41,7 @@ const containerDom = document.getElementById('container');
 
 for(let i = 0; i < teamArray.length; i++){
     containerDom.innerHTML += `<div class="card">
-                                    <div id="img"><img src="img/${teamArray[i].foto}" alt="foto"></div>
-                                    <div id="text">${teamArray[i].nome} ${teamArray[i].ruolo}</div>
+                                    <img src="img/${teamArray[i].foto}" alt="foto">
+                                    <div class="text"><h2>${teamArray[i].nome}</h2> <span>${teamArray[i].ruolo}</span></div>
                                 </div>`
-    
-
 }  
-
-console.log(teamArray[0].nome + teamArray[0].ruolo)
